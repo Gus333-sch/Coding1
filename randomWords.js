4901209fc8eeffd9cbb048ae9e1d07c760c8551c
Gus333-sch <gustawjanluberadzki@students.brillantmont.ch>
2026-09-08 14:01:31 +0200

    Implement random word generator function
    Add function to generate and display a random word

--- randomWords.js
+++ randomWords.js
@@ -1,1 +1,10 @@
+const words = ["apple", "banana", "cherry", "orange", "abricot", "grape", "kiwi"];
 
+function generateRandomWord() {
+  
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const randomWord = words[randomIndex];
+
+  document.getElementById("wordDisplay").innerText = randomWord;
+
+}
